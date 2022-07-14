f87a114cfa0d73c485ba4a3cdb2cb60f1b8cd5f7
Kamula <kamulaisaac@gmail.com>
2022-07-14 08:32:33 +0300

    monthly and total payment currency formating

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
     private static final int MONTHS_IN_A_YEAR = 12;
     public static void main(String[] args) {
-//        get input from user: PRINCIPLE,ANNUAL_INTEREST_RATE & TERM_IN_YEARS
-//        P = PRINCIPLE,
+//        get input from user: PRINCIPAL,ANNUAL_INTEREST_RATE & TERM_IN_YEARS
+//        P = PRINCIPAL,
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the principle amount");
-        double principle = scanner.nextDouble();
+        System.out.println("Enter the principal amount");
+        double principal = scanner.nextDouble();
 
         System.out.println("Enter the annual interest rate");
         float annual_interest_rate = scanner.nextFloat();
@@ -19,10 +20,12 @@ public class Main {
         int number_of_payments = term_in_years * MONTHS_IN_A_YEAR;
 
 //        formula implementation
-        double monthly_payment = principle * (
+        double monthly_payment = principal * (
                 (monthly_interest_rate * (Math.pow(1+monthly_interest_rate,number_of_payments)))/
                         ((Math.pow(1+monthly_interest_rate,number_of_payments)) -1)
                 );
-        System.out.println("Monthly payment: "+ monthly_payment);
+//        print and format monthly payment & Total payment
+        System.out.println("Monthly payment: "+ NumberFormat.getCurrencyInstance().format(monthly_payment));
+        System.out.println("Total payment: "+ NumberFormat.getCurrencyInstance().format(monthly_payment*number_of_payments));
     }
 }
