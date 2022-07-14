3312044eac46e4f196473474585fe96eb6506dcd
Kamula <kamulaisaac@gmail.com>
2022-07-14 07:58:29 +0300

    get principle annual interest rate & term in years from user

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        get input from user: PRINCIPLE,ANNUAL_INTEREST_RATE & TERM_IN_YEARS
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the principle amount");
+        double principle = scanner.nextDouble();
+
+        System.out.println("Enter the annual interest rate");
+        float annual_interest_rate = scanner.nextFloat();
+
+        System.out.println("Enter loan term in years");
+        int term_in_years = scanner.nextInt();
     }
 }
